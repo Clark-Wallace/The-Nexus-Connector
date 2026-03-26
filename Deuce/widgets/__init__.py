cd411aafaa5f9ac164585bf96d494141dc0ab3b3
Clark Wallace <Clarkwallace.iv@gmail.com>
2026-03-25 22:28:24 -0700

    remove: Deuce moved to standalone repo at github.com/Clark-Wallace/Deuce

--- Deuce/widgets/__init__.py
+++
@@ -1,5 +0,0 @@
-from .chat_panel import ChatPanel
-from .action_ledger import ActionLedger
-from .file_browser import FileBrowser
-from .confirm_dialog import ConfirmDialog
-from .provider_switcher import ProviderSwitcher
